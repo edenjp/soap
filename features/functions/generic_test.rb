08de9636c06513ccfd58d8f72e4c64e4fafc82ab
Éden Pereira <edenjp@hotmail.com>
2018-11-18 19:56:31 -0200

    moving method

--- features/functions/generic_test.rb
+++ features/functions/generic_test.rb
@@ -1,10 +1,10 @@
 def action(table)
-  @parameters                       = table.rows_hash  
+  @parameters       = table.rows_hash  
   
   puts "\n\nPRINTING GIVEN BODY\n\n"
   puts cep_xml_payload
 
-  endpoint               = $api['url']
+  endpoint          = $api['url']
 
   @action = HTTParty.post(
     endpoint,:headers => {"Content-Type" => 'text/xml', "SOAPAction" => 'urn:CEPServiceAction'}, 
@@ -14,7 +14,17 @@ def action(table)
   puts @action.code
   puts "\n\nPRINTING ENDPOINT\n\n"
   puts endpoint
+end
 
-
-
+def cep_xml_payload
+  '<x:Envelope xmlns:x="http://schemas.xmlsoap.org/soap/envelope/" xmlns:www="urn:http://www.byjg.com.br">
+      <x:Header/>
+      <x:Body>
+          <www:obterLogradouroAuth>
+            <www:cep>'+@parameters['cep']+'</www:cep>
+            <www:usuario>'+@parameters['user']+'</www:usuario>
+            <www:senha>'+@parameters['password']+'</www:senha>
+          </www:obterLogradouroAuth>
+      </x:Body>
+  </x:Envelope>'  
 end
